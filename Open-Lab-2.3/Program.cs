aeb5af8feaa89c5c31b5bcf5cfcb3f922fc849d0
z18ista08 <z18ista08@Z00P1-18.SPSKNM>
2019-10-14 08:58:11 +0200

    Add project files.

---
+++ Open-Lab-2.3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Open_Lab_2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = { 1, 2, 3, 4, 5 };
+            int a = array1[4] % array1[2];
+            Console.WriteLine(a);
+        }
+    }
+}
